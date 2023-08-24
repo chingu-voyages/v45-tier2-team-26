35d15b246524c04d5fcf23f379033cc8c214138b
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-08-24 13:39:24 -0400

    Update getLocation.js
    Updated to include the reverse geocode URL (accidentally had the forward geocode URL previously, which was causing the error)

--- src/getLocation.js
+++ src/getLocation.js
@@ -1,68 +1,66 @@
-export default function getLocation(latLongData) {
-  const url = 'https://api.geoapify.com/v1/batch/geocode/search?apiKey=143adca609dd41258606ce840f8db559';
-  //   const body = JSON.stringify(latLongData);
-  //   console.log(latLongData[0]);
-  //   console.log(body[0]);
+export default async function getLocation(latLongData) {
+  const url = 'https://api.geoapify.com/v1/batch/geocode/reverse?apiKey=143adca609dd41258606ce840f8db559';
 
-  // I'm stuck here - following the sample code (pasted below), when I stringify the latLongData,
-  // I get the error: body[0] must be a string. And when I don't stringify the data, I get the error
-  // "Unexpected token o in JSON at position 1"
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(latLongData),
+  //     });
 
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(latLongData),
-  })
-    .then(getBodyAndStatus)
-    .then((result) => {
-      if (result.status !== 202) {
-        return Promise.reject(result);
-      }
-      console.log(`Job ID: ${result.body.id}`);
-      console.log(`Job URL: ${result.body.url}`);
-      return getAsyncResult(`${url}&id=${result.body.id}`, 1000, 100).then((queryResult) => {
-        console.log(queryResult);
-        return queryResult;
-      });
-    })
-    .catch((err) => console.log(err));
+  //     const result = await getBodyAndStatus(response);
 
-  function getBodyAndStatus(response) {
-    return response.json().then((responceBody) => ({
-      status: response.status,
-      body: responceBody,
-    }));
-  }
+  //     if (result.status !== 202) {
+  //       throw result;
+  //     }
 
-  function getAsyncResult(url, timeout, maxAttempt) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        repeatUntilSuccess(resolve, reject, 0);
-      }, timeout);
-    });
+  //     console.log(`Job ID: ${result.body.id}`);
+  //     console.log(`Job URL: ${result.body.url}`);
 
-    function repeatUntilSuccess(resolve, reject, attempt) {
-      console.log(`Attempt: ${attempt}`);
-      fetch(url)
-        .then(getBodyAndStatus)
-        .then((result) => {
-          if (result.status === 200) {
-            resolve(result.body);
-          } else if (attempt >= maxAttempt) {
-            reject('Max amount of attempts achived');
-          } else if (result.status === 202) {
-            // Check again after timeout
-            setTimeout(() => {
-              repeatUntilSuccess(resolve, reject, attempt + 1);
-            }, timeout);
-          } else {
-            // Something went wrong
-            reject(result.body);
-          }
-        })
-        .catch((err) => reject(err));
-    }
-  }
+  //     const queryResult = await getAsyncResult(`${url}&id=${result.body.id}`, 1000, 100);
+  //     console.log(queryResult);
+  //     return queryResult;
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // async function getBodyAndStatus(response) {
+  //   const responseBody = await response.json();
+  //   return {
+  //     status: response.status,
+  //     body: responseBody,
+  //   };
+  // }
+
+  // async function getAsyncResult(url, timeout, maxAttempt) {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       repeatUntilSuccess(resolve, reject, 0);
+  //     }, timeout);
+  //   });
+
+  //   async function repeatUntilSuccess(resolve, reject, attempt) {
+  //     console.log(`Attempt: ${attempt}`);
+  //     try {
+  //       const response = await fetch(url);
+  //       const result = await getBodyAndStatus(response);
+
+//       if (result.status === 200) {
+//         resolve(result.body);
+//       } else if (attempt >= maxAttempt) {
+//         reject('Max amount of attempts achieved');
+//       } else if (result.status === 202) {
+//         setTimeout(() => {
+//           repeatUntilSuccess(resolve, reject, attempt + 1);
+//         }, timeout);
+//       } else {
+//         reject(result.body);
+//       }
+//     } catch (error) {
+//       reject(error);
+//     }
+//   }
 }
