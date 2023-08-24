005319369579e75f4916ca1333891c75b3ab3d87
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-08-24 10:59:10 -0400

    Update DetailData.jsx
    Map over meteor data to create a new array of objects formatted as [{lon: , lat: }] for use in the reverse geocoding api

--- src/DetailData.jsx
+++ src/DetailData.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 function DetailData() {
   const [didRender, setDidRender] = useState(false);
-  const [meteorData, setMeteorData] = useState({})
+  const [meteorData, setMeteorData] = useState({});
+  const [latLongData, setLatLongData] = useState({});
 
-  // useEffect to run once - at first render of DetailData
+  // useEffect to run once - at first render of DetailData.
+  // Calls the async function getMeteorData
   useEffect(() => {
     if (didRender === false) {
       getMeteorData();
@@ -14,12 +16,19 @@ function DetailData() {
     }
   }, []);
 
+  // fetch the meteor data and set it to meteorData state
   async function getMeteorData() {
-    const meteorData = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
+    const data = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
       .then((response) => response.json());
-    console.log(meteorData);
-    setMeteorData(meteorData)
+    setMeteorData(data);
+    const latLongData = data.map((meteor) => ({ lon: meteor.reclong, lat: meteor.reclat }));
+    console.log(data[0], latLongData[0]);
   }
+
+  // fetch("https://api.geoapify.com/v1/geocode/reverse?lat=51.21709661403662&lon=6.7782883744862374&apiKey=143adca609dd41258606ce840f8db559", requestOptions)
+  //   .then(response => response.json())
+  //   .then(result => console.log(result))
+  //   .catch(error => console.log('error', error));
 
   return (
     <p>Detail data component</p>
