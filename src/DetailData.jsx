9409234e2fe2a87ec1aa0180b0cf9e871181811b
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-08-24 09:40:28 -0400

    Update DetailData.jsx
    Added fetch for meteor strike data and and useEffect to try to limit the number of times fetch is called to one. However, it still seems to get called twice? I think the fetch needs to be moved to the App.jsx component anyway because all the child components need it

--- src/DetailData.jsx
+++ src/DetailData.jsx
@@ -1,7 +1,27 @@
-function DetailData {
-    return (
+import React, { useState, useEffect } from 'react';
 
-    )
+function DetailData() {
+  const [didRender, setDidRender] = useState(false);
+
+  // useEffect to run once - at first render of DetailData
+  useEffect(() => {
+    if (didRender === false) {
+      getMeteorData();
+      return () => {
+        setDidRender(true);
+      };
+    }
+  }, []);
+
+  async function getMeteorData() {
+    const meteorData = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
+      .then((response) => response.json());
+    console.log(meteorData);
+  }
+
+  return (
+    <p>Detail data component</p>
+  );
 }
 
 export default DetailData;
