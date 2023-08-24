83a72eea398d4d427308ca73b138de0b359e9a94
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-08-24 09:39:22 -0400

    Update App.jsx
    Added DetailData component import and rendering to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import DetailData from './DetailData';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -33,6 +34,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <DetailData />
     </div>
   );
 }
