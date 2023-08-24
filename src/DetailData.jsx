fe11e926c6ab9da3445eb98156dbb37ad05d300d
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-08-24 10:11:38 -0400

    Update DetailData.jsx
    Added state for the fetched meteor data

--- src/DetailData.jsx
+++ src/DetailData.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function DetailData() {
   const [didRender, setDidRender] = useState(false);
+  const [meteorData, setMeteorData] = useState({})
 
   // useEffect to run once - at first render of DetailData
   useEffect(() => {
@@ -17,6 +18,7 @@ function DetailData() {
     const meteorData = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
       .then((response) => response.json());
     console.log(meteorData);
+    setMeteorData(meteorData)
   }
 
   return (
