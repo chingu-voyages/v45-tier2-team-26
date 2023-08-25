a5c676bc61c84add862223fa4f183a05b3a3c0ad
wilson <wilson1.chu@gmail.com>
2023-08-25 00:57:16 -0400

    small refactor for AirBnB

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
