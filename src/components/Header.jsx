07793ab915c079a40827778db7839e7bcd8b6ebd
wilson <wilson1.chu@gmail.com>
2023-08-27 03:18:59 -0400

    added placeholders for input boxes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,39 +13,46 @@ export default function Header() {
       </section>
 
       <section className="inputFields">
-
         <section className="columnGroup">
           <div className="fieldGroup" id="name">
             <label htmlFor="">Name:</label>
-            <input type="text" />
+            <input type="text" placeholder="e.g. Acapulco" />
           </div>
           <div className="fieldGroup" id="year">
             <label htmlFor="">Year:</label>
-            <input type="text" />
+            <input type="text" placeholder="e.g. 1914" />
           </div>
         </section>
 
         <section className="columnGroup">
           <div className="fieldGroup" id="composition">
             <label htmlFor="">Composition:</label>
-            <input type="text" />
+            <input type="text" placeholder="e.g. L6" />
           </div>
           <div className="fieldGroup" id="range">
             <label htmlFor="">Mass Range:</label>
 
             <div className="sliderGroup">
-              <input type="range" min="0" max="100" onChange={handleSliderChange} className="rangeSlider" />
+              <input
+                type="range"
+                min="0"
+                max="100"
+                onChange={handleSliderChange}
+                className="rangeSlider"
+              />
               <p className="sliderValue">{`${sliderValue} Meters`}</p>
             </div>
-
           </div>
-
         </section>
       </section>
 
       <section className="headerBtns">
-        <button type="button" className="searchBtn">Search</button>
-        <button type="button" className="clearBtn">Clear</button>
+        <button type="button" className="searchBtn">
+          Search
+        </button>
+        <button type="button" className="clearBtn">
+          Clear
+        </button>
       </section>
     </header>
   );
