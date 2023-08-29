93567241f2c3cea816245417d500dfb34ee28c29
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-08-29 13:18:40 -0400

    Update publicAPI.js
    Reformatted fetchMeteorData

--- src/services/publicAPI.js
+++ src/services/publicAPI.js
@@ -1,4 +1,10 @@
-export default async () => fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
-  .then((response) => response.json())
-  .then((data) => data)
-  .catch((error) => console.error(error));
+export default async function fetchMeteorData() {
+  try {
+    const response = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error; // Re-throw the error to allow the caller to handle it
+  }
+}
