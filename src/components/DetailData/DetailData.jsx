4158b9cee6e78bf98b98dfa280ca34e57ab079b3
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-08-29 11:09:54 -0400

    Update DetailData.jsx
    Updated to match how the summary data component pulls in meteorData. Also moved the latLongData functionality out to a separate function. Commented out some functionality for testing

--- src/components/DetailData/DetailData.jsx
+++ src/components/DetailData/DetailData.jsx
@@ -1,65 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import getLocation from '../../getLocation';
+import React, { useState, useEffect, useMemo } from 'react';
+import getLocation from './getLocation';
+import getMeteoriteData from '../../services/publicAPI';
 
 function DetailData() {
-  const [didRender, setDidRender] = useState(false);
-  const [fullMeteorData, setFullMeteorData] = useState({});
+  // This state is for testing purposes only. It will be replaced by props later.
+  const [meteorData, setMeteorData] = useState(null);
+  const locationData = useMemo(() => getLocation(meteorData), [meteorData]);
 
-  // useEffect to run once - at first render of DetailData.
-  // Calls the async function getMeteorData
   useEffect(() => {
-    if (didRender === false) {
-      getMeteorData();
-      return () => {
-        setDidRender(true);
-      };
-    }
+    console.log('testing');
+    getMeteoriteData().then((data) => setMeteoriteData(data.slice(0, 1000)));
   }, []);
 
-  // fetch the meteor data and set it to meteorData state
-  async function getMeteorData() {
-    try {
-      const response = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json');
-      let data = await response.json();
-      data = data.slice(0, -900);// Currently only reverse geocoding for the first 100 lat/lon pairs. There is an error happening because not all meteor strikes have a lat/lon. This exits the whole fetch request in getLocation. So maybe we need to subset latLongData to exclude NaNs? Then we can use locations.query.lon and locations.query.lat to match back up the locations to the original meteor data?
-
-      // Create lon/lat data formatted for batch reverse geocoding with the Geoapify API
-      const latLongData = data.map((meteor) => (
-        {
-          lon: parseFloat(meteor.reclong),
-          lat: parseFloat(meteor.reclat),
-        }
-      ));
-
-      // Fetch locations from the Geoapify API
-      const locations = await getLocation(latLongData);
-      console.log(locations);
-
-      // Create a map from locationData using latitude and longitude as the key
-      const locationMap = new Map();
-      locations.forEach((location) => {
-        const key = `${location.query.lon},${location.query.lat}`;
-        locationMap.set(key, { city: location.city, country: location.country });
-      });
-
-      // Update meteorData with location information
-      data.forEach((meteor) => {
-        const key = `${parseFloat(meteor.reclong)},${parseFloat(meteor.reclat)}`;
-        const location = locationMap.get(key);
-        if (location) {
-          meteor.geolocation = `${location.city}, ${location.country}`;
-        }
-      });
-
-      setFullMeteorData(data);
-    } catch (error) {
-      console.error(error);
+  function locations(data) {
+    if (!data) {
+      return null;
     }
+    return getLocation(data);
   }
+
+  console.log(locations(meteorData));
+
+  // // fetch the meteor data and set it to meteorData state
+  // async function getMeteorData() {
+  //   try {
+  //     const response = await fetch('https://data.nasa.gov/resource/gh4g-9sfh.json');
+  //     let data = await response.json();
+  //     data = data.slice(0, -900);// Currently only reverse geocoding for the first 100 lat/lon pairs. There is an error happening because not all meteor strikes have a lat/lon. This exits the whole fetch request in getLocation. So maybe we need to subset latLongData to exclude NaNs? Then we can use locations.query.lon and locations.query.lat to match back up the locations to the original meteor data?
+
+  //     // Create lon/lat data formatted for batch reverse geocoding with the Geoapify API
+  //     const latLongData = data.map((meteor) => (
+  //       {
+  //         lon: parseFloat(meteor.reclong),
+  //         lat: parseFloat(meteor.reclat),
+  //       }
+  //     ));
+
+  //     // Fetch locations from the Geoapify API
+  //     const locations = await getLocation(latLongData);
+  //     console.log(locations);
+
+  //     // Create a map from locationData using latitude and longitude as the key
+  //     const locationMap = new Map();
+  //     locations.forEach((location) => {
+  //       const key = `${location.query.lon},${location.query.lat}`;
+  //       locationMap.set(key, { city: location.city, country: location.country });
+  //     });
+
+  //     // Update meteorData with location information
+  //     data.forEach((meteor) => {
+  //       const key = `${parseFloat(meteor.reclong)},${parseFloat(meteor.reclat)}`;
+  //       const location = locationMap.get(key);
+  //       if (location) {
+  //         meteor.geolocation = `${location.city}, ${location.country}`;
+  //       }
+  //     });
+
+  //     setFullMeteorData(data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
   return (
     <div>
-      {fullMeteorData.length
+      {/* {fullMeteorData.length
         ? (
           <div>
             <h1>Detail Data</h1>
@@ -92,7 +97,7 @@ function DetailData() {
               </tbody>
             </table>
           </div>
-        ) : <p>Content loading...</p>}
+        ) : <p>Content loading...</p>} */}
     </div>
   );
 }
