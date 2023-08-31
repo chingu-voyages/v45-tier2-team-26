06f81c569240316ffc37005152b498d6d0407ba5
VictorCam <campav@oregonstate.edu>
2023-08-31 18:01:57 -0500

    json_data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import SummaryMetric from './components/SummaryMetric/SummaryMetric';
 import Header from './components/Header/Header';
+import json from '../Meteorite_Landings.json';
 
 function App() {
+  useEffect(() => {
+    console.log('loaded data', JSON.stringify(json));
+  });
+
   return (
     <div className="App">
       <Header />
