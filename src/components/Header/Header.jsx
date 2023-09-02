4d8fde017b1232d63f47acfe03b257cd37236164
dejourford <dejourford@my.unt.edu>
2023-09-02 12:20:56 -0500

    reformat header for responsive navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react';
 import './header.css';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleHideMenu = () => {
+    console.log('hide menu');
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleShowMenu = () => {
+    console.log('show menu');
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const [sliderValue, setSliderValue] = useState(50);
   const handleSliderChange = (e) => {
     setSliderValue(e.target.value);
@@ -32,57 +43,123 @@ export default function Header() {
 
   return (
     <header>
-      <section className="logo">
-        <h1>FireBall</h1>
-      </section>
+      <nav>
+        <section className={`mobileNav ${isMenuOpen ? 'open' : ''}`}>
+          <section className="logo">
+            <h1>FireBall</h1>
+            <button type="button" id="closeNavBtn" onClick={handleHideMenu}>
+              <div className="bar closeBtnBar" id="closeBtnBar1" />
+              <div className="bar closeBtnBar" id="closeBtnBar2" />
+            </button>
+          </section>
 
-      <section className="inputFields">
-        <section className="columnGroup">
-          <div className="fieldGroup" id="name">
-            <label htmlFor="">Name:</label>
-            <input
-              type="text"
-              placeholder="e.g. Acapulco"
-              onChange={handleNameChange}
-              value={name}
-            />
-          </div>
-          <div className="fieldGroup" id="year">
-            <label htmlFor="">Year:</label>
-            <input type="text" placeholder="e.g. 1914" value={year} onChange={(e) => setYear(e.target.value)} />
-          </div>
+          <section className="inputFields" id="mobileInputFields">
+            <section className="columnGroup">
+              <div className="fieldGroup mobileFieldGroup" id="name">
+                <label htmlFor="">Name:</label>
+                <input
+                  type="text"
+                  placeholder="e.g. Acapulco"
+                  onChange={handleNameChange}
+                  value={name}
+                />
+              </div>
+              <div className="fieldGroup mobileFieldGroup" id="year">
+                <label htmlFor="">Year:</label>
+                <input type="text" placeholder="e.g. 1914" value={year} onChange={(e) => setYear(e.target.value)} />
+              </div>
+
+              <div className="fieldGroup mobileFieldGroup" id="composition">
+                <label htmlFor="">Composition:</label>
+                <input type="text" placeholder="e.g. L6" value={composition} onChange={(e) => setComposition(e.target.value)} />
+              </div>
+              <div className="fieldGroup mobileFieldGroup" id="range">
+                <label htmlFor="">Mass Range:</label>
+
+                <div className="sliderGroup">
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    onChange={handleSliderChange}
+                    className="rangeSlider"
+                  />
+                  <p className="sliderValue">{`${sliderValue} Meters`}</p>
+                </div>
+              </div>
+            </section>
+          </section>
+
+          <section className="headerBtns" id="mobileHeaderBtns">
+            <button type="button" className="searchBtn" onClick={handleSearch}>
+              Search
+            </button>
+            <button type="button" className="clearBtn" onClick={handleClear}>
+              Clear
+            </button>
+          </section>
+
         </section>
+        <section className="mainNav">
+          <section className="logo">
+            <button type="button" className="hamburgerMenu" onClick={handleShowMenu}>
+              <div className="bar" />
+              <div className="bar" />
+              <div className="bar" />
+            </button>
+            <h1>FireBall</h1>
+          </section>
 
-        <section className="columnGroup">
-          <div className="fieldGroup" id="composition">
-            <label htmlFor="">Composition:</label>
-            <input type="text" placeholder="e.g. L6" value={composition} onChange={(e) => setComposition(e.target.value)} />
-          </div>
-          <div className="fieldGroup" id="range">
-            <label htmlFor="">Mass Range:</label>
+          <section className="inputFields" id="mainInputFields">
+            <section className="columnGroup">
+              <div className="fieldGroup" id="name">
+                <label htmlFor="">Name:</label>
+                <input
+                  type="text"
+                  placeholder="e.g. Acapulco"
+                  onChange={handleNameChange}
+                  value={name}
+                />
+              </div>
+              <div className="fieldGroup" id="year">
+                <label htmlFor="">Year:</label>
+                <input type="text" placeholder="e.g. 1914" value={year} onChange={(e) => setYear(e.target.value)} />
+              </div>
+            </section>
 
-            <div className="sliderGroup">
-              <input
-                type="range"
-                min="0"
-                max="100"
-                onChange={handleSliderChange}
-                className="rangeSlider"
-              />
-              <p className="sliderValue">{`${sliderValue} Meters`}</p>
-            </div>
-          </div>
+            <section className="columnGroup">
+              <div className="fieldGroup" id="composition">
+                <label htmlFor="">Composition:</label>
+                <input type="text" placeholder="e.g. L6" value={composition} onChange={(e) => setComposition(e.target.value)} />
+              </div>
+              <div className="fieldGroup" id="range">
+                <label htmlFor="">Mass Range:</label>
+
+                <div className="sliderGroup">
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    onChange={handleSliderChange}
+                    className="rangeSlider"
+                  />
+                  <p className="sliderValue">{`${sliderValue} Meters`}</p>
+                </div>
+              </div>
+            </section>
+          </section>
+
+          <section className="headerBtns" id="mainHeaderBtns">
+            <button type="button" className="searchBtn" onClick={handleSearch}>
+              Search
+            </button>
+            <button type="button" className="clearBtn" onClick={handleClear}>
+              Clear
+            </button>
+          </section>
         </section>
-      </section>
+      </nav>
 
-      <section className="headerBtns">
-        <button type="button" className="searchBtn" onClick={handleSearch}>
-          Search
-        </button>
-        <button type="button" className="clearBtn" onClick={handleClear}>
-          Clear
-        </button>
-      </section>
     </header>
   );
 }
