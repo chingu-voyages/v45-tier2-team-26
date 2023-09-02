d379ff026eb3262d60ce2b80ad2e0ebc3fc2d4a7
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-01 21:37:00 -0400

    Update DetailData.jsx
    Changed to work with Radar API, and to store location data when paging through the table in locationMemo

--- src/components/DetailData/DetailData.jsx
+++ src/components/DetailData/DetailData.jsx
@@ -2,68 +2,97 @@ import './DetailData.css';
 import React, { useState, useEffect } from 'react';
 import getMeteorData from '../../services/publicAPI';
 import getLocation from './getLocation';
-import fetchLocation from './fetchLocation';
-import getDetailData from './getDetailData';
 
 function DetailData() {
-  const itemsPerPage = 10; // You can adjust this as needed
-  const [meteorData, setMeteorData] = useState(null);
+  const itemsPerPage = 10;
+  const [meteorProps, setMeteorProps] = useState(null); // this will be removed when we have props
   const [currentPage, setCurrentPage] = useState(1);
   const [endIndex, setEndIndex] = useState(null);
-  const [detailData, setDetailData] = useState(null);
+  const [tableData, setTableData] = useState(null);
+  const [locationMemo, setLocationMemo] = useState({});
 
-  // Note - this might change - meteorData might be brought in via props.
-  // If so, also remove meteorData state
+  // Note - planning for meteor data to be brought in via props.
   useEffect(() => {
+    // this will be removed when we have props
     async function fetchData() {
       const data = await getMeteorData();
-      console.log(data);
-      setMeteorData(data);
+      setMeteorProps(data);
     }
     fetchData();
   }, []);
 
   useEffect(() => {
-    if (meteorData) {
-      console.log(fetchLocation());
-      // const [startIndex, currentEndIndex] = getIndices(currentPage);
+    if (meteorProps) {
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const newEndIndex = startIndex + itemsPerPage > meteorProps.length - 1
+        ? meteorProps.length - 1
+        : startIndex + itemsPerPage;
+      setEndIndex(newEndIndex);
 
-      // const meteorDataSubset = meteorData.slice(startIndex, currentEndIndex);
+      const meteorSubset = meteorProps.slice(startIndex, newEndIndex + 1);
 
-      // async function fetchDisplayData() {
-      //   const locations = await getLocation(meteorDataSubset);
-      //   setDetailData(getDetailData(meteorDataSubset, locations));
-      // }
-      // fetchDisplayData();
+      const updatedMeteors = meteorSubset.map(async (meteor) => {
+        const { id, GeoLocation } = meteor;
+
+        // If GeoLocation doesn't exist or is an empty string,
+        // return data unavailable
+        if (!GeoLocation || GeoLocation === '') {
+          return {
+            ...meteor,
+            location: 'Data unavailable',
+          };
+        }
+        // If location data is memoized, use it
+        if (locationMemo[id]) {
+          return {
+            ...meteor,
+            location: locationMemo[id],
+          };
+        }
+
+        // Fetch the location data
+
+        const formattedGeoLocation = GeoLocation.replace(/[()]/g, '');
+
+        try {
+          const locationData = await getLocation(formattedGeoLocation);
+          let state = 'State unknown';
+          if (locationData.addresses[0].state) {
+            state = locationData.addresses[0].state;
+          }
+          setLocationMemo((prevLocationMemo) => ({
+            ...prevLocationMemo,
+            [id]: `${state}, ${locationData.addresses[0].country}`,
+          }));
+
+          return {
+            ...meteor,
+            location: `${locationData.addresses.state}, ${locationData.addresses.country}`,
+          };
+        } catch (error) {
+          // console.error('Error fetching location data:', error);
+          return {
+            ...meteor,
+            location: 'Error fetching location',
+          };
+        }
+      });
+
+      // Wait for all promises to resolve and then set table data
+      Promise.all(updatedMeteors).then((result) => {
+        setTableData(result);
+      });
     }
-  }, [meteorData, currentPage]);
-
-  // useEffect(() => {
-  //   if (locationsData) {
-  //     console.log('location data updated');
-  //     setDetailData(getDetailData(meteorData, locationsData));
-  //   }
-  // }, [locationsData]);
+  }, [meteorProps, currentPage, locationMemo]);
 
   const handlePageChange = (pageNumber) => {
     const newPage = pageNumber;
     setCurrentPage(newPage);
-    const [startIndex, newEndIndex] = getIndices(newPage);
-    setEndIndex(newEndIndex);
   };
-
-  function getIndices(page) {
-    const startIndex = (page - 1) * itemsPerPage;
-
-    const newEndIndex = ((startIndex + itemsPerPage) > (meteorData.length - 1))
-      ? (meteorData.length - 1) : (startIndex + itemsPerPage);
-
-    return [startIndex, newEndIndex];
-  }
 
   return (
     <div className="detailContainer">
-      {detailData ? (
+      {tableData ? (
         <div>
           <h1>Detail Data</h1>
           <div className="tableContainer">
@@ -73,24 +102,24 @@ function DetailData() {
                   <th>Name</th>
                   <th>ID</th>
                   <th>Composition</th>
-                  <th>Mass</th>
+                  <th>Mass (grams)</th>
                   <th>Year</th>
                   <th>Latitude</th>
                   <th>Longitude</th>
-                  <th>City, State, Country</th>
+                  <th>State, Country</th>
                 </tr>
               </thead>
               <tbody>
-                {detailData.map((item) => (
+                {tableData.map((item) => (
                   <tr key={item.id}>
                     <td>{item.name}</td>
                     <td>{item.id}</td>
                     <td>{item.recclass}</td>
-                    <td>{item.mass}</td>
+                    <td>{item.massgrams}</td>
                     <td>{item.year}</td>
                     <td>{parseFloat(item.reclat).toFixed(3)}</td>
                     <td>{parseFloat(item.reclong).toFixed(3)}</td>
-                    <td>{item.geolocation}</td>
+                    <td>{item.location}</td>
                   </tr>
                 ))}
               </tbody>
@@ -103,7 +132,7 @@ function DetailData() {
               {' '}
               {currentPage}
             </span>
-            <button disabled={endIndex >= meteorData.length - 1} onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+            <button disabled={endIndex >= meteorProps.length - 1} onClick={() => handlePageChange(currentPage + 1)}>Next</button>
           </div>
         </div>
       ) : (<p>Content loading...</p>)}
